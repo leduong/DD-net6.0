eb6abf14cce93a49f21d860d54dc6f866b60252f
Le Duong <dangduong2401@gmail.com>
2024-03-14 07:33:46 +0700

    API Task - Code for Test

---
+++ App.Application/ApplicationDIConfig.cs
@@ -0,0 +1,21 @@
+﻿using App.Application.Services.Common;
+using App.Infrastructure.Persistence.Repositories;
+using App.Infrastructure.ServiceAgents;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Application
+{
+    public class ApplicationDIConfig
+    {
+        public static void AddConfig(IServiceCollection services)
+        {
+            services.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
+
+        }
+    }
+}
